79e3d53768f0a9e0d5345496b7f5d0a18216bf83
Alex <alexandru.cehan@gmail.com>
2022-01-17 18:05:34 +0200

    Initial commit. Closes #1

---
+++ src/main/java/guru/springframework/spring5jokesv2/Spring5JokesV2Application.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5jokesv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5JokesV2Application.class, args);
+	}
+
+}
